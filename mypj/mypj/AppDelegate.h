cff1fc0223b3c97c75c23fd858345e71a437c9fd
xyfqldy <1399140589@qq.com>
2015-12-04 11:52:11 +0800

    咱们第一次上交

---
+++ mypj/mypj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mypj
+//
+//  Created by jiuxiaoming on 15/12/4.
+//  Copyright © 2015年 jiuxiaoming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mypj/mypjTests/mypjTests.m
@@ -0,0 +1,39 @@
+//
+//  mypjTests.m
+//  mypjTests
+//
+//  Created by jiuxiaoming on 15/12/4.
+//  Copyright © 2015年 jiuxiaoming. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface mypjTests : XCTestCase
+
+@end
+
+@implementation mypjTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
